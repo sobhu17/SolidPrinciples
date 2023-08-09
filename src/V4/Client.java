e2643a08afaaa74b238fdf8c8b9bfdec6afe163f
saurabh kaushik <habsaurabh1702@gmail.com>
2023-08-09 14:58:54 +0530

    SOLID PRINCIPLES

--- src/V4/Client.java
+++ src/V4/Client.java
@@ -4,7 +4,17 @@ public class Client {
     public static void main(String[] args) {
         // final version for bird
 
+        /*
+            here we have make use of all the SOLID principles and design this bird feature
 
+            in last version we have implemented Dependency Inversion - D of SOLID principles
+
+            which says that not to concrete classes can be directly depend on each other,
+            they should be connected via an interface.
+
+            Dependency Injection - Instead of creating an object inside a class what we can do is
+            send the object as a parameter in a constructor that is known as dependency injection
+         */
 
     }
 }
